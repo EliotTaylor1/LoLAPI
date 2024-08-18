3679eafbff25abfc84a66365c67d8571bd40862d
EliotTaylor1 <104195627+EliotTaylor1@users.noreply.github.com>
2024-08-18 13:23:10 +0100

    Add run.sh for easy running on Unix systems

---
+++ run.sh
@@ -0,0 +1,21 @@
+echo Checking for existing venv..
+if [ -d ".venv" ]; then
+    echo venv exists already
+else
+    echo venv doesnt exist. Creating venv,
+    python3 -m venv .venv
+fi
+
+echo Activating venv
+source ./.venv/bin/activate
+
+echo Checking pip up to date...
+python3 -m pip install --upgrade pip
+python3 -m pip --version
+
+echo Checking required packages...
+python3 -m pip install -r requirements.txt
+echo Packages checked
+
+echo Starting...
+python3 src/main.py
